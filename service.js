cf365e24c20e6802fc13b2cc8f11f6cb02d9b9ff
Steven Nagie <sanagie@yahoo.com>
2016-10-27 11:49:08 -0600

    All done.

--- service.js
+++ service.js
@@ -1,10 +1,19 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+    var deferred = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
-    })
-  }
+    }).then(function(response) {
+      // This line parses our data out, so that when we resolve our deferred object we can do so with easy to read data. This cleans up our code in the controller.
+      var parsedResponse = response.data.data;
+      for (var i = 0; i < parsedResponse.length; i++) {
+        parsedResponse[i].first_name = "Ralf";
+      }
+      deferred.resolve(parsedResponse);
+    });
+    return deferred.promise;
+  };
 
 });
